855925d097c31058b3e872d1e66f8695afdf3348
Tanjina99 <144571293+Tanjina99@users.noreply.github.com>
2025-02-16 11:48:34 -0500

    tanjina work

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div className='text-center'>Home</div>
-  )
-}
+  return <div className="text-center">Welcome to dinning hub</div>;
+};
 
-export default Home
+export default Home;
